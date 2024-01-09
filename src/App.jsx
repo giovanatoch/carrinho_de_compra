083cab81f8bcca171e9d86854e400776999419a6
giovanatoch <tochtrop.giovana@gmail.com>
2024-01-09 10:53:27 -0300

    Add código

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Products from './components/Products/Products';
+import Provider from './context/Provider';
+import Cart from './components/Cart/Cart';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-    Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-    Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Header />
+      <Products />
+      <Cart />
+    </Provider>
   );
 }
 
